d4bd2dca3d3c3ff6ff4faee460066ac0ef410ce0
Jamin Desktop <jamin.kauf@yahoo.de>
2022-06-01 16:27:33 +0200

    refactoring for prettiness

--- pyLegoMario/mario_GUI.py
+++ pyLegoMario/mario_GUI.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 from pathlib import Path
 import asyncio
-from typing import Union
+from PIL import ImageTk, Image
 from .mario import Mario
 from .LEGO_MARIO_DATA import *
-from PIL import ImageTk, Image
 
 class MarioWindow(tk.Frame):
     def __init__(self, mario_entity: Mario, master=None):
-        self._mario = mario_entity
+        self.mario = mario_entity
         tk.Frame.__init__(self, tk.Toplevel() if tk._default_root else master)
         # Window Setup
         self.master.minsize(644, 165)
@@ -46,11 +45,11 @@ class MarioWindow(tk.Frame):
         self.y_acceleration_box.grid(row=1, column=1)
         self.z_acceleration_box.grid(row=1, column=2)
         # Hook Fields to Mario
-        self._mario.AddAccelerometerHook(self.input_acceleration_data)
+        self.mario.add_accelerometer_hooks(self.input_acceleration_data)
         # End Acceleration Data
 
         # Start Pants Data
-        # Static container (invisible frame) and descriptive label
+        # Static container (invisible frame) and description label
         self.pants_frame = tk.Frame(self)
         self.pants_label = tk.Label(self.pants_frame, text="Pants")
         # Variable and Entry for Displaying Variable
@@ -63,7 +62,7 @@ class MarioWindow(tk.Frame):
         self.pants_label.grid(row=0)
         self.pants_box.grid(row=1)
         # Hook Field to Mario
-        self._mario.AddPantsHook(self.input_pants_data)
+        self.mario.add_pants_hooks(self.input_pants_data)
         # End Pants Data
 
         # Start RGB Data
@@ -78,7 +77,7 @@ class MarioWindow(tk.Frame):
         self.rgbLabel.grid(row=0)
         self.rgbBox.grid(row=1)
         # Hook Field to Mario
-        self._mario.AddTileHook(self.input_rgb_data)
+        self.mario.add_tile_hooks(self.input_rgb_data)
         # End RGB Data
 
         # Scale for Adjusting Volume
@@ -94,7 +93,7 @@ class MarioWindow(tk.Frame):
         self.logText = tk.StringVar()
         self.logBox = tk.Text(self, state=tk.DISABLED, width=80)
         self.logBox.grid(row=1, columnspan=6, sticky=tk.NSEW)
-        self._mario.AddLogHook(self.input_log_data)
+        self.mario.add_log_hooks(self.input_log_data)
 
         # Start Buttons
         # Button Container
@@ -107,9 +106,9 @@ class MarioWindow(tk.Frame):
                                         command=self.dis_connect_mario)
         self.connectButton.grid(row=0, column=0)
 
-        self.quitButton = tk.Button(self.buttonFrame, text='Quit',
+        self.quit_button = tk.Button(self.buttonFrame, text='Quit',
                                     command=self.quit)
-        self.quitButton.grid(row=0, column=1)
+        self.quit_button.grid(row=0, column=1)
 
         self.turnOffButton = tk.Button(self.buttonFrame, text="Turn Off",
                                         command=self.turn_mario_off)
@@ -157,42 +156,48 @@ class MarioWindow(tk.Frame):
         self.request_port_button = tk.Button(self.portFrame, 
             text="Request Value", command=self.request_port)
         # Radiobutton for Each Port (Grid All to portFrame)
-        for port in (1,2,3,4,6): # no Port 0 because accelerometer messages aren't printed to Log, no Port 5 because it doesn't exist
-            tk.Radiobutton(self.portFrame, variable=self.portVar, value=port, text="Port %s" % port, command=self.updateModeMenu).grid(row=(port-1 )//3, column=(port-1)%3 if port!= 6 else 1)
+        # no Port 0/5 (accelerometer isn't printed to Log, Port 5 doesn't exist)
+        for i, port in enumerate((1,2,3,4,6)):
+            radiobutton = tk.Radiobutton(self.portFrame, 
+                            variable=self.portVar, value=port,
+                            text=f"Port {port}", command=self.update_mode_menu)
+            radiobutton.grid(row=i // 3, column=i % 3)
         self.portFrame.grid(row=2, column=3)
         self.request_port_button.grid(row=1, column=2)
 
         # display and update the window
         asyncio.get_event_loop().create_task(self.run_window())
 
-    def updateModeMenu(self) -> None:
-        """Update the mode menu for setting port format with the available
-        port modes for each port. Called every time a different port gets selected.
+    def update_mode_menu(self) -> None:
+        """Update the port mode menu with the available port modes for the
+        selected port. Called every time a different port gets selected.
         """
-        # check which modes exist for selected port
         new_choices = VALID_PORT_MODES[self.portVar.get()]
-        
+
         # change menu accordingly
         self.mode_menu['menu'].delete(0, tk.END)
         for choice in new_choices:
-            self.mode_menu['menu'].add_command(label=str(choice), command=tk._setit(self.port_mode_variable, str(choice)))
-        
-        # only reset selection if previously selected port is not valid anymore
+            self.mode_menu['menu'].add_command(
+                label=str(choice), 
+                command=tk._setit(self.port_mode_variable, str(choice)))
+
+        # only reset selection if previously selected mode is not valid
         if not int(self.port_mode_variable.get()) in new_choices:
             self.port_mode_variable.set("0")
 
     def set_port_input_format(self) -> None:
-        self._mario.port_setup(port=self.portVar.get(), mode=self.port_mode_variable.get(),
-                                notifications=self.notificationVar.get())
+        self.mario.port_setup(port=self.portVar.get(), 
+                              mode=self.port_mode_variable.get(),
+                              notifications=self.notificationVar.get())
 
     def dis_connect_mario(self) -> None:
         """Connects Mario if Mario isn't running. Disconnect Mario if Mario is
         connected. Passes otherwise (e.g. running but not connected yet)
         """
-        if not self._mario._run:
-            asyncio.create_task(self._mario.connect())
-        elif self._mario._client:
-            asyncio.create_task(self._mario.disconnect())
+        if not self.mario.run:
+            asyncio.create_task(self.mario.connect())
+        elif self.mario.client:
+            asyncio.create_task(self.mario.disconnect())
 
     def set_mario_volume(self, placeholder):
         """Creates asyncio task of Mario's set_volume Coroutine.
@@ -202,59 +207,59 @@ class MarioWindow(tk.Frame):
             placeholder (any): This argument is NOT used! It exists only for
                 compatibility with tk.Scale's way of calling its command.
         """
-        self._mario.defaultVolume = int(placeholder)
-        self._mario.set_volume(self._mario.defaultVolume)
+        self.mario.default_volume = int(placeholder)
+        self.mario.set_volume(self.mario.default_volume)
     
     def set_auto_reconnect(self):
         """Sets Mario's ._autoReconnect attribute to the current value of 
         self.reconnectVar (determined by self.reconnectCheckBox)."""
-        self._mario._autoReconnect = bool(self.reconnectVar.get())
+        self.mario.auto_reconnect = bool(self.reconnectVar.get())
 
     def turn_mario_off(self):
         """Creates asyncio task of Mario's .turn_off() Coroutine.
         """
-        asyncio.create_task(self._mario.turn_off())
+        asyncio.create_task(self.mario.turn_off())
 
     def request_port(self) -> None:
         """Requests the currently selected port of Mario.
         """
         port_id = self.portVar.get()
         loop = asyncio.get_event_loop()
-        loop.create_task(self._mario.request_port_value(port_id))
-    
+        loop.create_task(self.mario.request_port_value(port_id))
+
     def input_log_data(self, sender: Mario, msg: str) -> None:
         """Function to display log messages in GUI
 
         Args:
-            sender (Mario): The Mario entity that sends the data. Argument is only here for compatibility with mario's event hooks.
+            sender (Mario): The Mario entity that sends the data.
+                Used for compatibility with Mario's event hooks.
             msg (str): Log Message
         """
-        assert sender == self._mario
+        assert sender == self.mario
         try:
-            # format message nicely by splitting pure message (Hexadecimal) and human readable string
             content, hex_msg = msg.split(", Hex: ")
             msg = f"{content.ljust(self.logBox['width'] - len(hex_msg))}{hex_msg}"
         except ValueError:
             # unable to split/format: leave message untouched
             pass
-        # Insert Message Into Log Textbox (excluding acc data)
-        if not msg.startswith("X: "): #and "port 3" in msg.lower()
+        # Insert Message Into Log Textbox (excluding acceleration data)
+        if not msg.startswith("X: "):  # and "port 3" in msg.lower()
             self.logBox['state'] = tk.NORMAL
             self.logBox.insert(tk.END, f"\n{msg}")
             self.logBox['state'] = tk.DISABLED
-            self.logBox.see(tk.END) # scroll down
+            self.logBox.see(tk.END)  # scroll down
 
     def input_acceleration_data(self, sender: Mario, x: int, y: int, z: int) -> None:
         """Hook for acceleration data to be displayed on GUI
 
         Args:
-            sender (Mario): The Mario entity that sends the data. 
+            sender (Mario): The Mario entity that sends the data.
                 Only used for compatibility with mario's event hooks.
             x (int): acceleration data in x direction
             y (int): acceleration data in y direction
             z (int): acceleration data in z direction
         """
-        assert sender == self._mario
+        assert sender == self.mario
         self.x_acceleration_text.set(str(x))
         self.y_acceleration_text.set(str(y))
         self.z_acceleration_text.set(str(z))
@@ -267,7 +272,7 @@ class MarioWindow(tk.Frame):
                 Only used for compatibility with mario's event hooks.
             pants (str): The type of pants mario is wearing. See LEGO_MARIO_DATA.py for more info
         """
-        assert sender == self._mario
+        assert sender == self.mario
         self.pants_text.set(pants)
 
     def input_rgb_data(self, sender: Mario, color_or_tile: str) -> None:
@@ -278,7 +283,7 @@ class MarioWindow(tk.Frame):
                 Only used for compatibility with mario's event hooks.
             color_or_tile (str): String of the color or tile. See LEGO_MARIO_DATA.py for more info
         """
-        assert sender == self._mario
+        assert sender == self.mario
         self.rgbText.set(color_or_tile)
 
     def quit(self) -> None:
@@ -286,10 +291,10 @@ class MarioWindow(tk.Frame):
         Mario remains connected. Call Mario.disconnect() to disconnect.
         """
         # Remove Hooks from Mario
-        self._mario.RemoveEventsHook((
+        self.mario.remove_hooks((
             self.input_acceleration_data, self.input_pants_data, 
             self.input_rgb_data, self.input_log_data))
-        self._mario._autoReconnect = False
+        self.mario.auto_reconnect = False
         # Close Window
         try:
             self.master.destroy()
@@ -297,29 +302,32 @@ class MarioWindow(tk.Frame):
             pass
 
     async def run_window(self, interval: float = 0.06) -> None:
-        """Endless loop that keeps the window running, updating it every INTERVAL seconds.\n
-        This loop is also responsible for enabling and disabling GUI functions depending on Mario's connection status
+        """Endless loop that keeps the window running, updating it every
+        INTERVAL seconds.\n
+        This loop is also responsible for enabling and disabling GUI functions
+        depending on Mario's connection status
 
         Args:
-            interval (float, optional): Time interval between window updates. Defaults to 0.06.
+            interval (float, optional): Time interval between window updates.
+            Defaults to 0.06.
         """
         try:
             while True:
                 # Mario is connected and running
-                if self._mario._client:
+                if self.mario.is_connected:
                     self.request_port_button.config(state=tk.NORMAL)
                     self.portFormatButton.config(state=tk.NORMAL)
                     self.turnOffButton.config(state=tk.NORMAL)
                     self.connectButton.config(text="Disconnect", state=tk.NORMAL)
-                    self.master.title("Lego Mario - %s" % self._mario._client.address)
+                    self.master.title(f"Lego Mario - {self.mario.client.address}")
                 # Mario is disconnected and not trying to connect
-                elif not self._mario._run:
+                elif not self.mario.run:
                     self.request_port_button.config(state=tk.DISABLED)
                     self.portFormatButton.config(state=tk.DISABLED)
                     self.turnOffButton.config(state=tk.DISABLED)
                     self.master.title("Lego Mario - Not Connected")
                     self.connectButton.config(text="Connect", state=tk.NORMAL)
-                # Mario is running, but not connected (currently trying to connect)
+                # Mario is running, but not connected (trying to connect)
                 else:
                     self.request_port_button.config(state=tk.DISABLED)
                     self.portFormatButton.config(state=tk.DISABLED)
