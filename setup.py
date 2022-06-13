6d6a3363d801daf417c42e935ad0c9c60b64522b
Jamin Desktop <jamin.kauf@yahoo.de>
2022-06-13 16:16:16 +0200

    fixed classifiers

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ setup(
     author='Jamin Kauf, Bruno Hautzenberger',
     author_email='jamin.kauf@yahoo.de',
     description='Module for handling connections with the Lego Mario toy',
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['bleak', 'pathlib', 'asyncio', 'pillow', 'pygame'],
     keywords=['lego', 'python', 'super mario', 'lego mario', 'bluetooth'],
     classifiers=[
-        'Development Status :: Done',
-        'Intended Audience :: Programmers',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
         'Operating System :: Microsoft :: Windows',
-        'License :: MIT License'
+        'License :: OSI Approved :: MIT License'
     ],
     python_requires='>=3.9',
     project_urls = {'repository': r'https://github.com/Jackomatrus/pyLegoMario'},
